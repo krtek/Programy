15ce414309b1d8d32b7db05d57f1027a2c2eb443
Lukas Marek <lukas.marek@gmail.com>
2011-05-22 10:47:25 +0200

    Initial commit

---
+++ src/cz/hackathon/programy/Festivals.java
@@ -0,0 +1,13 @@
+package cz.hackathon.programy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Festivals extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
