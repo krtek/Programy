e639ca7e8f4387efad75503d0633a646d278fe34
Lukas Marek <lukas.marek@gmail.com>
2011-05-22 12:01:19 +0200

    Pridane fieldy

--- src/cz/hackathon/programy/dto/Stages.java
+++ src/cz/hackathon/programy/dto/Stages.java
@@ -1,9 +1,23 @@
 package cz.hackathon.programy.dto;
 
+import java.util.List;
+
 /**
  * User: lukas.marek@cleverlance.com
  * Date: 22.5.11
  * Time: 11:48
  */
 public class Stages {
+    public String name;
+    public String desc;
+    public List<StageEvent> events;
+
+    @Override
+    public String toString() {
+        return "Stages{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", events=" + events +
+                '}';
+    }
 }
