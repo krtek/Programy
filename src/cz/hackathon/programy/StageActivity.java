6c4a76cfa5191830127663c502b4292f687061fa
kouba-prog <kouba159@gmail.com>
2011-05-23 15:28:44 +0200

    edit caption notice event => event.name

--- src/cz/hackathon/programy/StageActivity.java
+++ src/cz/hackathon/programy/StageActivity.java
@@ -96,7 +96,11 @@ public class StageActivity extends ListActivity {
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
 		StageEvent event = list.get(((AdapterContextMenuInfo)menuInfo).position);
-		menu.add(Menu.NONE, ADD_NOTIFICATION, 0, "Pridat upozorneni na "+event).setIcon(android.R.drawable.ic_dialog_alert);
+		if (event != null) {
+			menu.add(Menu.NONE, ADD_NOTIFICATION, 0,
+					"Pridat upozorneni na " + event.name).setIcon(
+					android.R.drawable.ic_dialog_alert);
+		}
 		// menu.add(Menu.NONE, PREFS_MENU, 1,
 		// R.string.preferences).setIcon(android.R.drawable.ic_menu_preferences);
 		// super.onCreateContextMenu(menu, v, menuInfo);
@@ -106,7 +110,9 @@ public class StageActivity extends ListActivity {
 	public boolean onContextItemSelected(MenuItem item) {
 		if (ADD_NOTIFICATION==item.getItemId()) {			
 			StageEvent event = list.get(((AdapterContextMenuInfo) item.getMenuInfo()).position);
-			Toast.makeText(this, "Upozorneni na "+event, 5000).show();
+			if (event != null) {
+				Toast.makeText(this, "Upozorneni na " + event.name, 5000).show();
+			}
 		}
 		return true;
 	}
